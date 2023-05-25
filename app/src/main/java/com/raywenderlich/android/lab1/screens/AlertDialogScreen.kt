74f58e300d5109d5201c198abd0fe8fe32fb8691
Nastya <nastyremez8439@gmail.com>
2023-05-25 20:29:41 +0300

    AlertDialogScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
@@ -1,6 +1,15 @@
 package com.raywenderlich.android.lab1.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -17,5 +26,14 @@ fun AlertDialogScreen() {
 
 @Composable
 fun MyAlertDialog() {
-    //TODO add your code here
+    Column(modifier = Modifier.fillMaxWidth(1f)) {
+        Text(
+            modifier = Modifier.fillMaxWidth(1f),
+            text = "Тест текста после нажатия кнопки", fontSize = 28.sp,
+            fontStyle = FontStyle.Normal
+            , fontWeight = FontWeight.W700
+            , fontFamily = FontFamily.Monospace
+            , textAlign = TextAlign.End
+        )
+    }
 }
