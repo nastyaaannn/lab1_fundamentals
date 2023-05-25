fd19bea13f1dff1f775c8de27431a484d58a5c74
Nastya <nastyremez8439@gmail.com>
2023-05-25 20:18:25 +0300

    TextFieldScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/TextFieldScreen.kt
@@ -3,9 +3,17 @@ package com.raywenderlich.android.lab1.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -27,5 +35,17 @@ fun TextFieldScreen() {
 
 @Composable
 fun MyTextField() {
-    //TODO add your code here
+    val message = remember {
+        mutableStateOf("")
+    }
+    Column() {
+        Text(text = message.value, fontSize = 28.sp)
+        TextField(
+            label = { Text(text = "Поле для ввода") },
+            value = message.value,
+            textStyle = TextStyle(fontSize = 25.sp),
+            onValueChange = {newText->message.value = newText},
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone)
+        )
+    }
 }
