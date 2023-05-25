c43de0df3514a463c84db1aef982ce7ffe4986e0
Nastya <nastyremez8439@gmail.com>
2023-05-25 20:23:52 +0300

    ProgressIndicatorsScreen.kt

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ProgressIndicatorsScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTargetMarker
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import androidx.compose.runtime.ComposableTarget as ComposableTarget1
 
 @Composable
 fun ProgressIndicatorScreen() {
@@ -18,7 +20,7 @@ fun ProgressIndicatorScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        //TODO add your code here
+
     }
 
     BackButtonHandler {
